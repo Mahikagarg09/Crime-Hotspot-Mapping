e00f7062641e623774ac540553b00fbbc5c314f9
Mahikagarg09 <mg@gwdcollective.com>
2024-11-10 23:08:15 +0530

    remove error after submitting form

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -131,7 +131,7 @@ const Form = () => {
             </label>
             <div className="flex gap-2">
               <input
-                value={selectedLocation.address}
+                value={selectedLocation ? selectedLocation.address : ""}
                 placeholder="Click 'Select Location' to choose on map"
                 className="flex-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                 readOnly
